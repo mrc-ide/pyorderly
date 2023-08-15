37bc6b2d4d4d1bb2af3966e2c9c947ba496ac221
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-08-15 09:11:31 +0100

    Add manual time conversion functions

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,11 @@
-from outpack.util import find_file_descend, iso_time_str
+import datetime
+
+from outpack.util import (
+    find_file_descend,
+    iso_time_str,
+    num_to_time,
+    time_to_num,
+)
 
 
 def test_find_descend(tmp_path):
@@ -13,3 +20,12 @@ def test_find_descend(tmp_path):
 def test_convert_iso_time():
     t = 1691686967.895351
     assert iso_time_str(t) == "20230810-170247"
+
+
+def test_convert_num_to_time():
+    t = 1691686967.895351
+    x = datetime.datetime(
+        2023, 8, 10, 17, 2, 47, 895351, tzinfo=datetime.timezone.utc
+    )
+    assert time_to_num(x) == t
+    assert num_to_time(t) == x
