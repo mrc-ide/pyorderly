4007921f469ce1f719ecb3edd3462fe6755bc23c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-08-16 11:25:48 +0100

    Implement basic init method

---
+++ src/outpack/init.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from outpack.config import Config, read_config, write_config
+
+
+def outpack_init(
+    path,
+    *,
+    path_archive="archive",
+    use_file_store=False,
+    require_complete_tree=False,
+):
+    path = Path(path)
+    if path.exists():
+        if not path.is_dir():
+            msg = f"Path '{path}' already exists but is not a directory"
+            raise Exception(msg)
+        # As in orderly2, there are unresolved questions about if we
+        # allow initialising an outpack root into an existing
+        # directory. Here, we'll allow it.
+
+    config = Config.new(
+        path_archive=path_archive,
+        use_file_store=use_file_store,
+        require_complete_tree=require_complete_tree,
+    )
+
+    path_outpack = path.joinpath(".outpack")
+
+    if path_outpack.exists():
+        _validate_same_configuration(config, read_config(path))
+    else:
+        path_outpack.mkdir(parents=True, exist_ok=True)
+        path_outpack.joinpath("metadata").mkdir(parents=True, exist_ok=True)
+        path_outpack.joinpath("location").mkdir(parents=True, exist_ok=True)
+        path_outpack.joinpath("location/local").mkdir(
+            parents=True, exist_ok=True
+        )
+        write_config(config, path)
+
+    return path
+
+
+def _validate_same_configuration(now, then):
+    if now.core == then.core:
+        return
+    err = [
+        f"* {f} was {getattr(then, f)} but {getattr(now, f)} requested"
+        for f in now.fields()
+        if getattr(now, f) != getattr(then, f)
+    ]
+    msg = "Trying to change configuration when re-initialising:\n" + "\n".join(
+        err
+    )
+    raise Exception(msg)
