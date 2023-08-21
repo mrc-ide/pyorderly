93b101d6c362492362691b80900d630515863667
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-08-21 17:57:16 +0100

    Test working dir behaviour

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+import pytest
+
 from outpack.config import read_config
 from outpack.filestore import FileStore
 from outpack.index import Index
 from outpack.init import outpack_init
 from outpack.root import root_open
+from outpack.util import transient_working_directory
 
 
 def test_can_open_existing_root():
@@ -24,3 +28,18 @@ def test_can_open_root_from_subdir():
     r = root_open("example", False)
     assert root_open("example/src", True).path == r.path
     assert root_open("example/src/data", True).path == r.path
+
+
+def test_can_open_root_from_wd():
+    p = Path("example").resolve()
+    with transient_working_directory("example"):
+        assert root_open(None, True).path == p
+        assert root_open(None, False).path == p
+
+
+def test_can_open_root_from_subdir():
+    p = Path("example").resolve()
+    with transient_working_directory("example/src/data"):
+        assert root_open(None, True).path == p
+        with pytest.raises(Exception, match="Did not find existing outpack"):
+            assert root_open(None, False).path == p
