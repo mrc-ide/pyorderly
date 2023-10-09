2bee363a8ae8cb63850320d067e0b99e23bda285
Rich FitzJohn <rich.fitzjohn@gmail.com>
2023-10-09 18:33:48 +0100

    Fix rounding bug in id generation

--- src/outpack/ids.py
+++ src/outpack/ids.py
@@ -1,3 +1,4 @@
+import math
 import re
 import secrets
 import time
@@ -8,13 +9,16 @@ RE_ID = re.compile("^[0-9]{8}-[0-9]{6}-[0-9a-f]{8}$")
 
 
 def fractional_to_bytes(x):
-    return f"{round((x % 1) * pow(256, 2)):04x}"
+    return f"{math.floor((x % 1) * pow(256, 2)):04x}"
 
 
 def outpack_id():
     t = time.time()
     rand = secrets.token_hex(2)
-    return f"{iso_time_str(t)}-{fractional_to_bytes(t)}{rand}"
+    id = f"{iso_time_str(t)}-{fractional_to_bytes(t)}{rand}"
+    if len(id) != 24:
+        breakpoint()
+    return id
 
 
 def is_outpack_id(x: str):
