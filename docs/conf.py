7d745d577619c29341967c6e1284df55ca73b38c
Mantra <mantrakusumgar@outlook.com>
2024-01-31 12:36:53 +0000

    testing workflow

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'outpack-py'
+copyright = '2024, Rich FitzJohn'
+author = 'Rich FitzJohn'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_rtd_theme",
+    'autoapi.extension',
+    "myst_parser",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_copybutton"
+]
+
+autoapi_dirs = ['../src']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.md
@@ -0,0 +1,18 @@
+# Welcome to outpack-py's documentation!
+
+```{toctree}
+:maxdepth: 2
+:caption: 'Contents:'
+
+readme
+code
+```
+
+% uses MyST markdown format https://jupyterbook.org/en/stable/content/myst.html
+
+Also inserting a reference to a function defined in the code file [here](add_function).
+
+Alternative way of referencing is {ref}`Add Function` (each section has an automatic ref generated for it,
+it matches the name of the section).
+
+We can also link a document, here I will link {doc}`readme`
