95a5d2d634336beab2450927e59af9d3800a6cff
Mantra <mantrakusumgar@outlook.com>
2024-01-31 12:42:36 +0000

    lint

--- docs/conf.py
+++ docs/conf.py
@@ -6,30 +6,30 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'outpack-py'
-copyright = '2024, Rich FitzJohn'
-author = 'Rich FitzJohn'
-release = '0.0.1'
+project = "outpack-py"
+copyright = "2024, Rich FitzJohn"
+author = "Rich FitzJohn"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx_rtd_theme",
-    'autoapi.extension',
+    "autoapi.extension",
     "myst_parser",
     "sphinx.ext.autosectionlabel",
-    "sphinx_copybutton"
+    "sphinx_copybutton",
 ]
 
-autoapi_dirs = ['../src']
+autoapi_dirs = ["../src"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
